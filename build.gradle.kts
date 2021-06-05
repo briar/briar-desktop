20e1e6c4a3f1d46fde54b0af764ffaf15adbe1fc
Nico Alt <nicoalt@posteo.org>
2021-06-05 15:19:39 +0200

    Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,46 @@
+import org.jetbrains.compose.compose
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.30"
+    kotlin("kapt") version "1.3.72"
+    id("org.jetbrains.compose") version "0.3.1"
+    id("java")
+    id("idea")
+}
+
+group = "app.briar.compose"
+version = "0.1"
+
+repositories {
+    jcenter()
+    mavenCentral()
+    maven { url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev") }
+}
+
+dependencies {
+    testImplementation(kotlin("test-testng"))
+    implementation(compose.desktop.currentOs)
+    implementation(project(path = ":briar:briar-core", configuration = "default"))
+    implementation(project(path = ":briar:bramble-java", configuration = "default"))
+}
+
+tasks.test {
+    useTestNG()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "11"
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "Briar"
+            packageVersion = "1.0.0"
+        }
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,15 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        maven { url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev") }
+    }
+    
+}
+rootProject.name = "Briar-compose"
+
+include("briar:bramble-api")
+include("briar:bramble-core")
+include("briar:bramble-java")
+include("briar:briar-api")
+include("briar:briar-core")
