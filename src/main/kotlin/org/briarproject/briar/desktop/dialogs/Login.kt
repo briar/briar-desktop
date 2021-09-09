e9c5338df1e3931c8c262faef0bbcedbb9d992f5
ialokim <ialokim@mailbox.org>
2021-09-09 12:13:18 +0200

    single-line password fields, define tab and enter actions
    fixes #22

--- src/main/kotlin/org/briarproject/briar/desktop/dialogs/Login.kt
+++ src/main/kotlin/org/briarproject/briar/desktop/dialogs/Login.kt
@@ -4,6 +4,8 @@ import androidx.compose.desktop.Window
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Button
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
@@ -11,10 +13,15 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.input.key.onKeyEvent
 import androidx.compose.ui.res.svgResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import org.briarproject.bramble.api.account.AccountManager
 import org.briarproject.bramble.api.lifecycle.LifecycleManager
+import java.awt.event.KeyEvent
 
 // TODO: Error handling
 fun Login(title: String, accountManager: AccountManager, lifecycleManager: LifecycleManager) =
@@ -44,15 +51,33 @@ private fun TheImage() {
 @Composable
 private fun TheTextField(accountManager: AccountManager, lifecycleManager: LifecycleManager) {
     var password by remember { mutableStateOf("") }
-    OutlinedTextField(password, { password = it }, label = { Text("Password") })
-    Spacer(Modifier.height(16.dp))
-    Button(onClick = {
+
+    val signIn = {
         accountManager.signIn(password)
 
         val dbKey = accountManager.databaseKey ?: throw AssertionError()
         lifecycleManager.startServices(dbKey)
         lifecycleManager.waitForStartup()
-    }) {
+    }
+
+    OutlinedTextField(
+        value = password,
+        onValueChange = { password = it },
+        label = { Text("Password") },
+        singleLine = true,
+        visualTransformation = PasswordVisualTransformation(),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password, imeAction = ImeAction.Done),
+        keyboardActions = KeyboardActions(onDone = { signIn() }),
+        modifier = Modifier.onKeyEvent {
+            if (it.nativeKeyEvent.keyCode == KeyEvent.VK_ENTER) {
+                signIn()
+                return@onKeyEvent true
+            }
+            false
+        }
+    )
+    Spacer(Modifier.height(16.dp))
+    Button(onClick = signIn) {
         Text("Login")
     }
 }
--- src/main/kotlin/org/briarproject/briar/desktop/dialogs/Registration.kt
+++ src/main/kotlin/org/briarproject/briar/desktop/dialogs/Registration.kt
@@ -4,6 +4,8 @@ import androidx.compose.desktop.Window
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Button
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
@@ -11,10 +13,18 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.input.key.onKeyEvent
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.svgResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import org.briarproject.bramble.api.account.AccountManager
 import org.briarproject.bramble.api.lifecycle.LifecycleManager
+import java.awt.event.KeyEvent
 
 // TODO: Error handling and password strength
 fun Registration(title: String, accountManager: AccountManager, lifecycleManager: LifecycleManager) =
@@ -45,15 +55,50 @@ private fun TheImage() {
 private fun TheTextFields(accountManager: AccountManager, lifecycleManager: LifecycleManager) {
     var username by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
-    OutlinedTextField(username, { username = it }, label = { Text("Username") })
-    OutlinedTextField(password, { password = it }, label = { Text("Password") })
-    Spacer(Modifier.height(16.dp))
-    Button(onClick = {
+
+    val focusManager = LocalFocusManager.current
+
+    val register = {
         accountManager.createAccount(username, password)
+
         val dbKey = accountManager.databaseKey ?: throw AssertionError()
         lifecycleManager.startServices(dbKey)
         lifecycleManager.waitForStartup()
-    }) {
+    }
+
+    OutlinedTextField(
+        value = username,
+        onValueChange = { username = it },
+        label = { Text("Username") },
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+        keyboardActions = KeyboardActions(onNext = { focusManager.moveFocus(FocusDirection.Down) }),
+        modifier = Modifier.onKeyEvent {
+            if (it.nativeKeyEvent.keyCode == KeyEvent.VK_TAB) {
+                focusManager.moveFocus(FocusDirection.Down)
+                return@onKeyEvent true
+            }
+            false
+        }
+    )
+    OutlinedTextField(
+        value = password,
+        onValueChange = { password = it },
+        label = { Text("Password") },
+        singleLine = true,
+        visualTransformation = PasswordVisualTransformation(),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password, imeAction = ImeAction.Done),
+        keyboardActions = KeyboardActions(onDone = { register() }),
+        modifier = Modifier.onKeyEvent {
+            if (it.nativeKeyEvent.keyCode == KeyEvent.VK_ENTER) {
+                register()
+                return@onKeyEvent true
+            }
+            false
+        }
+    )
+    Spacer(Modifier.height(16.dp))
+    Button(onClick = register) {
         Text("Register")
     }
 }
