9e6521bb35c3c5319d1122b836b966294163b9ac
Sebastian Kürten <sebastian@mobanisto.de>
2021-09-14 11:07:23 +0200

    Delete now-unused model classes Contact and Message

--- src/main/kotlin/org/briarproject/briar/desktop/paul/model/Contact.kt
+++
@@ -1,9 +0,0 @@
-package org.briarproject.briar.desktop.paul.model
-
-data class Contact(
-    val name: String,
-    val online: Boolean,
-    val profile_pic: String,
-    val last_heard: String,
-    val privateMessages: List<Message>
-)
