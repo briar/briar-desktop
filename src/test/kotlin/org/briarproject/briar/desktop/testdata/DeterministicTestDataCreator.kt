0aeac6bddbeea3903efebf31d59fccdd78c844e7
Sebastian Kürten <sebastian@mobanisto.de>
2021-11-25 10:12:48 +0100

    Improve formatting of KDoc in DeterministicTestDataCreator

--- src/test/kotlin/org/briarproject/briar/desktop/testdata/DeterministicTestDataCreator.kt
+++ src/test/kotlin/org/briarproject/briar/desktop/testdata/DeterministicTestDataCreator.kt
@@ -9,10 +9,9 @@ interface DeterministicTestDataCreator {
      * Create fake test data on the IoExecutor
      *
      * @param numContacts          Number of contacts to create. Must be >= 1
-     * @param numPrivateMsgs       Number of private messages to create for each
-     * contact.
-     * @param avatarPercent        Percentage of contacts
-     * that will use a random profile image. Between 0 and 100.
+     * @param numPrivateMsgs       Number of private messages to create for each contact.
+     * @param avatarPercent        Percentage of contacts that will use a random profile image.
+     *                             Between 0 and 100.
      * @param numPrivateGroups     Number of private groups to create. Must be >= 1
      * @param numPrivateGroupPosts Number of private group messages to create in each group
      */
