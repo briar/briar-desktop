7b41e5333ab1b870777c83e004a46b694d79db85
ialokim <ialokim@mailbox.org>
2022-06-22 16:54:21 +0200

    add copy buttons to website and email links

--- briar-desktop/src/main/kotlin/org/briarproject/briar/desktop/ui/AboutScreen.kt
+++ briar-desktop/src/main/kotlin/org/briarproject/briar/desktop/ui/AboutScreen.kt
@@ -1,5 +1,8 @@
 package org.briarproject.briar.desktop.ui
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.TooltipArea
+import androidx.compose.foundation.TooltipPlacement
 import androidx.compose.foundation.VerticalScrollbar
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -14,19 +17,25 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollbarAdapter
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ContentCopy
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.text
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
@@ -62,6 +71,7 @@ fun AboutScreen(
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun AboutScreen(modifier: Modifier = Modifier) {
     // sizes of the two columns
@@ -70,19 +80,26 @@ fun AboutScreen(modifier: Modifier = Modifier) {
     // format date
     val buildTime = Instant.ofEpochMilli(BuildData.GIT_TIME).atZone(ZoneId.systemDefault()).toLocalDateTime()
 
+    data class Entry(
+        val label: String,
+        val value: String,
+        val showCopy: Boolean = false
+    )
+
     // rows displayed in table
     val lines = buildList {
-        add(i18n("about.copyright") to "The Briar Project") // NON-NLS
-        add(i18n("about.license") to "GNU Affero General Public License v3") // NON-NLS
-        add(i18n("about.version") to BuildData.VERSION)
-        add(i18n("about.version.core") to BuildData.CORE_VERSION)
-        if (BuildData.GIT_BRANCH != null) add("Git branch" to BuildData.GIT_BRANCH) // NON-NLS
-        if (BuildData.GIT_TAG != null) add("Git tag" to BuildData.GIT_TAG) // NON-NLS
-        if (BuildData.GIT_BRANCH == null && BuildData.GIT_TAG == null) add("Git branch/tag" to "None detected") // NON-NLS
-        add("Git hash" to BuildData.GIT_HASH) // NON-NLS
-        add("Commit time" to DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(buildTime)) // NON-NLS
-        add(i18n("about.website") to "https://briarproject.org")
-        add(i18n("about.contact") to "desktop@briarproject.org") // NON-NLS
+        add(Entry(i18n("about.copyright"), "The Briar Project")) // NON-NLS
+        add(Entry(i18n("about.license"), "GNU Affero General Public License v3")) // NON-NLS
+        add(Entry(i18n("about.version"), BuildData.VERSION))
+        add(Entry(i18n("about.version.core"), BuildData.CORE_VERSION))
+        if (BuildData.GIT_BRANCH != null) add(Entry("Git branch", BuildData.GIT_BRANCH)) // NON-NLS
+        if (BuildData.GIT_TAG != null) add(Entry("Git tag", BuildData.GIT_TAG)) // NON-NLS
+        if (BuildData.GIT_BRANCH == null && BuildData.GIT_TAG == null)
+            add(Entry("Git branch/tag", "None detected")) // NON-NLS
+        add(Entry("Git hash", BuildData.GIT_HASH)) // NON-NLS
+        add(Entry("Commit time", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(buildTime))) // NON-NLS
+        add(Entry(i18n("about.website"), "https://briarproject.org", true))
+        add(Entry(i18n("about.contact"), "desktop@briarproject.org", true)) // NON-NLS
     }
 
     Column(modifier) {
@@ -111,7 +128,7 @@ fun AboutScreen(modifier: Modifier = Modifier) {
                 item {
                     HorizontalDivider()
                 }
-                items(lines) { (key, value) ->
+                items(lines) { (label, value, showCopy) ->
                     Row(
                         Modifier
                             .fillMaxWidth()
@@ -120,22 +137,60 @@ fun AboutScreen(modifier: Modifier = Modifier) {
                             .semantics(mergeDescendants = true) {
                                 // manual text setting can be removed if Compose issue resolved
                                 // https://github.com/JetBrains/compose-jb/issues/2111
-                                text = buildAnnotatedString { append("$key: $value") }
+                                text = buildAnnotatedString { append("$label: $value") }
                             }
                     ) {
                         Box(modifier = Modifier.weight(colSizes[0]).fillMaxHeight()) {
                             Text(
-                                text = key,
-                                modifier = Modifier.padding(8.dp)
+                                text = label,
+                                modifier = Modifier.padding(8.dp).align(Alignment.CenterStart)
                             )
                         }
                         VerticalDivider()
                         Box(modifier = Modifier.weight(colSizes[1]).fillMaxHeight()) {
-                            SelectionContainer {
-                                Text(
-                                    text = value,
-                                    modifier = Modifier.padding(8.dp)
-                                )
+                            Row(
+                                modifier = Modifier.fillMaxWidth(),
+                                horizontalArrangement = Arrangement.SpaceBetween,
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                SelectionContainer {
+                                    Text(
+                                        text = value,
+                                        modifier = Modifier.padding(8.dp)
+                                    )
+                                }
+                                if (showCopy) {
+                                    val clipboardManager = LocalClipboardManager.current
+                                    TooltipArea(
+                                        tooltip = {
+                                            Surface(
+                                                modifier = Modifier.shadow(4.dp),
+                                                shape = RoundedCornerShape(4.dp),
+                                            ) {
+                                                Text(
+                                                    text = i18n("copy"),
+                                                    modifier = Modifier.padding(8.dp)
+                                                )
+                                            }
+                                        },
+                                        delayMillis = 200,
+                                        tooltipPlacement = TooltipPlacement.ComponentRect(
+                                            alignment = Alignment.BottomCenter,
+                                        )
+                                    ) {
+                                        IconButton(
+                                            onClick = {
+                                                clipboardManager.setText(AnnotatedString(value))
+                                            }
+                                        ) {
+                                            Icon(
+                                                Icons.Filled.ContentCopy,
+                                                "copy",
+                                                modifier = Modifier.padding(8.dp)
+                                            )
+                                        }
+                                    }
+                                }
                             }
                         }
                     }
