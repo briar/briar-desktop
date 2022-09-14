a49c5f0f67e4224acde891ac7f25fb30683930da
Torsten Grote <t@grobox.de>
2022-09-14 13:38:17 -0300

    Implement forums empty and no selected state

--- briar-desktop/src/main/kotlin/org/briarproject/briar/desktop/forums/ForumsScreen.kt
+++ briar-desktop/src/main/kotlin/org/briarproject/briar/desktop/forums/ForumsScreen.kt
@@ -22,33 +22,62 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddComment
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import org.briarproject.briar.desktop.conversation.Explainer
+import org.briarproject.briar.desktop.ui.ColoredIconButton
 import org.briarproject.briar.desktop.ui.UiPlaceholder
 import org.briarproject.briar.desktop.ui.VerticalDivider
+import org.briarproject.briar.desktop.utils.InternationalizationUtils.i18n
 import org.briarproject.briar.desktop.viewmodel.viewModel
 
 @Composable
 fun ForumsScreen(
     viewModel: ForumsViewModel = viewModel(),
 ) {
-    Row(modifier = Modifier.fillMaxWidth()) {
-        ForumsList(
-            list = viewModel.groupList,
-            isSelected = viewModel::isSelected,
-            filterBy = viewModel.filterBy,
-            onFilterSet = viewModel::setFilterBy,
-            onGroupIdSelected = viewModel::selectGroup,
-            onAddButtonClicked = {},
-        )
-        VerticalDivider()
-        Column(modifier = Modifier.weight(1f).fillMaxHeight()) {
-            val id = viewModel.selectedGroupId.value
-            if (id != null) {
-                UiPlaceholder()
-            } else {
-                UiPlaceholder()
+    if (viewModel.groupList.value.isEmpty()) {
+        NoForumsYet {}
+    } else {
+        Row(modifier = Modifier.fillMaxWidth()) {
+            ForumsList(
+                list = viewModel.groupList,
+                isSelected = viewModel::isSelected,
+                filterBy = viewModel.filterBy,
+                onFilterSet = viewModel::setFilterBy,
+                onGroupIdSelected = viewModel::selectGroup,
+                onAddButtonClicked = {},
+            )
+            VerticalDivider()
+            Column(modifier = Modifier.weight(1f).fillMaxHeight()) {
+                val id = viewModel.selectedGroupId.value
+                if (id == null) {
+                    NoForumSelected()
+                } else {
+                    UiPlaceholder()
+                }
             }
         }
     }
 }
+
+@Composable
+fun NoForumsYet(onContactAdd: () -> Unit) = Explainer(
+    headline = i18n("welcome.title"),
+    text = i18n("forum.empty_state.text"),
+) {
+    ColoredIconButton(
+        icon = Icons.Filled.AddComment,
+        iconSize = 20.dp,
+        contentDescription = i18n("access.forums.add"),
+        onClick = onContactAdd,
+    )
+}
+
+@Composable
+fun NoForumSelected() = Explainer(
+    headline = i18n("forum.none_selected.title"),
+    text = i18n("forum.none_selected.hint"),
+)
