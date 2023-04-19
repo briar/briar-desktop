db65ba432553e7b801e3ebf976b5275422ed8c2e
Sebastian Kürten <sebastian@mobanisto.de>
2023-04-19 09:08:15 +0200

    Upgrade briar core and use macos onionwrapper artifacts

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,8 +38,8 @@ buildscript {
         set("dagger_version", "2.45")
         set("okhttp_version", "4.10.0")
         set("jackson_version", "2.13.4")
-        set("tor_version", "0.4.7.13")
-        set("obfs4proxy_version", "0.0.14")
+        set("tor_version", "0.4.7.13-2")
+        set("obfs4proxy_version", "0.0.14-tor2")
         set("snowflake_version", "2.5.1")
         set("jsoup_version", "1.15.3")
         set("bouncy_castle_version", "1.71")
@@ -47,6 +47,7 @@ buildscript {
         set("jmock_version", "2.12.0")
         set("mockwebserver_version", "4.10.0")
         set("onionwrapper_version", "0.0.2")
+        set("onionwrapper_version2", "0.0.2-macos")
     }
 }
 
