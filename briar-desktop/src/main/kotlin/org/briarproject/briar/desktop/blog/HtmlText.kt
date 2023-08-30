df220325099f80a14ab2f938fcdc2bbb5291058b
ialokim <ialokim@mailbox.org>
2023-08-30 13:08:23 +0200

    add support from strong, em, cite, sub, sup; as well as basic support for blockquotes and unordered lists (ul)

--- briar-desktop/src/main/kotlin/org/briarproject/briar/desktop/blog/HtmlText.kt
+++ briar-desktop/src/main/kotlin/org/briarproject/briar/desktop/blog/HtmlText.kt
@@ -21,15 +21,31 @@ package org.briarproject.briar.desktop.blog
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.ExperimentalTextApi
 import androidx.compose.ui.text.ParagraphStyle
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.drawText
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.rememberTextMeasurer
+import androidx.compose.ui.text.style.BaselineShift
 import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextIndent
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import org.briarproject.briar.desktop.utils.PreviewUtils.preview
 import org.intellij.lang.annotations.Language
 import org.jsoup.Jsoup
@@ -46,12 +62,17 @@ fun main() = preview {
 <h2>second headline</h2>
 <p>
     Hello World
-    <b>bold</b>, <i>italic</i>, <u>underline</u>, <b><i><u>all three</u></i></b>
+    <b>bold</b>, <i>italic</i>, <u>underline</u>, <strike>strikethrough</strike>, <b><i><u>all three <strike>or four</strike></u></i></b>
 </p>
 <p>
     This paragraph<br/>
     contains a <a href="https://google.com">link to Google</a>
 </p>
+<blockquote>This is a block quote<br/>with multiple lines.</blockquote>
+<ul>
+  <li>foo</li>
+  <li>bar</li>
+</ul>
 <p>
 <ol>
   <li>foo</li>
@@ -66,6 +87,7 @@ fun main() = preview {
 
 // This file is adapted from https://github.com/jeremyrempel/yahnapp (HtmlText.kt)
 
+@OptIn(ExperimentalTextApi::class)
 @Composable
 fun HtmlText(
     html: String,
@@ -77,12 +99,15 @@ fun HtmlText(
 
     // Elements we support:
     //    "h1", "h2", "h3", "h4", "h5", "h6",
-    //    "a", "b", "i", "u", "br", "p"
+    //    "a", "b"/"strong", "i"/"em"/"cite", "u", "strike", "sub", "sup", "q",
+    //    "br", "p", "blockquote",
+
+    // Elements that are currently only partially supported:
+    //    "ul" with "li": no nested lists, no "ul" within another paragraph "p"
 
     // Elements we still need to add support for:
-    //    "blockquote", "cite", "code", "dd", "dl", "dt", "em",
-    //    "li", "ol", "pre", "q", "small", "span", "strike", "strong", "sub",
-    //    "sup", "ul"
+    //    "code", "dd", "dl", "dt",
+    //    "ol", "pre", "small", "span",
 
     val h1 = MaterialTheme.typography.h1.toSpanStyle()
     val h2 = MaterialTheme.typography.h2.toSpanStyle()
@@ -93,26 +118,33 @@ fun HtmlText(
     val bold = SpanStyle(fontWeight = FontWeight.Bold)
     val italic = SpanStyle(fontStyle = FontStyle.Italic)
     val underline = SpanStyle(textDecoration = TextDecoration.Underline)
+    val strikethrough = SpanStyle(textDecoration = TextDecoration.LineThrough)
+    // todo: combination underline + strikethrough not working
+    val subscript = SpanStyle(baselineShift = BaselineShift.Subscript)
+    val superscript = SpanStyle(baselineShift = BaselineShift.Superscript)
     val link = SpanStyle(textDecoration = TextDecoration.Underline, color = MaterialTheme.colors.primaryVariant)
 
+    // todo: trim newlines / whitespaces?!
+    // todo: nested paragraphs not possible, but in HTML it is?
+    val blockquote = ParagraphStyle(textIndent = TextIndent(20.sp, 20.sp))
     val paragraph = ParagraphStyle()
 
     val formattedString = remember(html) {
         buildAnnotatedString {
-            var cursorPosition = 0
-            val appendAndUpdateCursor: (String) -> Unit = {
-                append(it)
-                cursorPosition += it.length
+            var cursorPosition = 0 // todo: couldn't this be replaced with this.length?
+            fun appendAndUpdateCursor(str: String) {
+                append(str)
+                cursorPosition += str.length
             }
 
-            val addParagraph: () -> Unit = {
+            fun addParagraph() {
                 pushStyle(paragraph)
                 if (cursorPosition > 0) {
                     appendAndUpdateCursor("\n")
                 }
             }
 
-            val addLink: (node: Element) -> Unit = { node ->
+            fun addLink(node: Element) {
                 val start = cursorPosition
                 val end = start + node.text().length
                 val href = node.attr("href")
@@ -124,6 +156,42 @@ fun HtmlText(
                     annotation = href
                 )
                 pushStyle(link)
+            }
+
+            fun startBlockQuote() {
+                pushStyle(blockquote)
+                pushStringAnnotation("quote", "")
+            }
+
+            fun endBlockQuote() {
+                pop()
+                pop()
+            }
+
+            // todo: this should be properly localized
+            fun startInlineQuote() {
+                appendAndUpdateCursor("\"")
+            }
+
+            fun endInlineQuote() {
+                appendAndUpdateCursor("\"")
+            }
+
+            fun startUnorderedList() {
+                pushStyle(blockquote)
+            }
+
+            fun endUnorderedList() {
+                pop()
+            }
+
+            fun startBullet() {
+                pushStringAnnotation("bullet", "")
+            }
+
+            fun endBullet() {
+                pop()
+                appendAndUpdateCursor("\n")
             }
 
             val doc = Jsoup.parse(html)
@@ -139,18 +207,33 @@ fun HtmlText(
 
                         is Element -> {
                             when (node.tagName()) {
+                                // headers
                                 "h1" -> pushStyle(h1)
                                 "h2" -> pushStyle(h2)
                                 "h3" -> pushStyle(h3)
                                 "h4" -> pushStyle(h4)
                                 "h5" -> pushStyle(h5)
                                 "h6" -> pushStyle(h6)
-                                "b" -> pushStyle(bold)
-                                "i" -> pushStyle(italic)
+
+                                // inline formatting
+                                "b", "strong" -> pushStyle(bold)
+                                "i", "em", "cite" -> pushStyle(italic)
                                 "u" -> pushStyle(underline)
-                                "br" -> appendAndUpdateCursor("\n")
-                                "p" -> addParagraph()
+                                "strike" -> pushStyle(strikethrough)
+                                "sub" -> pushStyle(subscript)
+                                "sup" -> pushStyle(superscript)
+                                "q" -> startInlineQuote()
                                 "a" -> addLink(node)
+
+                                // lists
+                                "ul" -> startUnorderedList()
+                                "li" -> startBullet()
+
+                                // misc
+                                "br" -> appendAndUpdateCursor("\n")
+                                "blockquote" -> startBlockQuote()
+                                "p" -> addParagraph()
+                                // else -> throw Exception("Unsupported tag '${node.tagName()}'")
                             }
                         }
 
@@ -163,7 +246,17 @@ fun HtmlText(
                 override fun tail(node: Node, depth: Int) {
                     if (node is Element) {
                         when (node.tagName()) {
-                            "h1", "h2", "h3", "h4", "h5", "h6", "b", "i", "u", "a", "p" -> pop()
+                            "h1", "h2", "h3", "h4", "h5", "h6",
+                            "b", "strong", "i", "em", "cite", "u", "strike", "sub", "sup",
+                            "a", "p",
+                            -> pop()
+
+                            "q" -> endInlineQuote()
+
+                            "ul" -> endUnorderedList()
+                            "li" -> endBullet()
+
+                            "blockquote" -> endBlockQuote()
                         }
                     }
                 }
@@ -171,9 +264,16 @@ fun HtmlText(
         }
     }
 
+    val textMeasurer = rememberTextMeasurer()
+    // todo: doesn't respect actual text size, but also cannot be changed currently
+    val textLayoutResult = remember { textMeasurer.measure("\u2022") }
+    val color = MaterialTheme.colors.onSurface
+
+    var onDraw: DrawScope.() -> Unit by remember { mutableStateOf({}) }
+
     ClickableText(
         text = formattedString,
-        modifier = modifier,
+        modifier = modifier.drawBehind { onDraw() },
         overflow = overflow,
         maxLines = maxLines,
         style = MaterialTheme.typography.body1.copy(color = MaterialTheme.colors.onSurface),
@@ -184,6 +284,50 @@ fun HtmlText(
                 ?.let { annotation ->
                     handleLink(annotation.item)
                 }
+        },
+        onTextLayout = { layoutResult ->
+            val quotes = formattedString.getStringAnnotations("quote").map {
+                val firstLine = layoutResult.getLineForOffset(it.start)
+                val lastLine = layoutResult.getLineForOffset(it.end - 1)
+                Rect(
+                    top = layoutResult.getLineTop(firstLine),
+                    bottom = layoutResult.getLineBottom(lastLine),
+                    left = 0f,
+                    right = 0f
+                )
+            }
+            val bullets = formattedString.getStringAnnotations("bullet").map {
+                val line = layoutResult.getLineForOffset(it.start)
+                Rect(
+                    top = layoutResult.getLineTop(line),
+                    bottom = layoutResult.getLineBottom(line),
+                    left = 0f,
+                    right = layoutResult.getLineLeft(line)
+                )
+            }
+            onDraw = {
+                quotes.forEach {
+                    val line = it.copy(left = it.left + 10.dp.toPx(), right = it.right + 11.dp.toPx())
+                    drawRect(
+                        color = Color.Green,
+                        topLeft = line.topLeft,
+                        size = line.size,
+                    )
+                }
+                bullets.forEach {
+                    drawText(
+                        textLayoutResult = textLayoutResult,
+                        color = color,
+                        topLeft = Offset(
+                            it.center.x - textLayoutResult.size.width / 2,
+                            it.center.y - textLayoutResult.size.height / 2,
+                        ),
+                    )
+                }
+            }
         }
     )
 }
+
+private fun AnnotatedString.getStringAnnotations(tag: String) =
+    getStringAnnotations(tag, 0, length)
