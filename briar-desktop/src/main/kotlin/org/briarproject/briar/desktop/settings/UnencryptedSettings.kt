0aa3415967d7fb7e83500925ccd8d77ab3ac6a6f
Sebastian Kürten <sebastian@mobanisto.de>
2024-08-14 15:07:54 +0200

    Make language PT_PT selectable in UI

--- briar-desktop/src/main/kotlin/org/briarproject/briar/desktop/settings/UnencryptedSettings.kt
+++ briar-desktop/src/main/kotlin/org/briarproject/briar/desktop/settings/UnencryptedSettings.kt
@@ -40,7 +40,7 @@ interface UnencryptedSettings : UnencryptedSettingsReadOnly {
 
         // languages as present in resources
         AR, BG, CA, CS, DE, ES, FA, FR, GL, HE, HU, IS, IT, JA, KA, KO, LT, MY, NB,
-        NL, PL, PT_BR, RO, RU, SK, SQ, SV, TR, UK, ZH_CN, ZH_TW;
+        NL, PL, PT_PT, PT_BR, RO, RU, SK, SQ, SV, TR, UK, ZH_CN, ZH_TW;
 
         val locale: Locale by lazy {
             if (this == DEFAULT)
