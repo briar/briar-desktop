cb9829428eca38fcdf95c91374fb089e307c41a9
Sebastian Kürten <sebastian@mobanisto.de>
2026-02-18 18:43:12 +0100

    Bump tor version, briar core and release date

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,7 @@ buildscript {
         set("dagger_version", "2.51.1")
         set("okhttp_version", "4.12.0")
         set("jackson_version", "2.13.4")
-        set("tor_version", "0.4.8.21")
+        set("tor_version", "0.4.8.22")
         set("lyrebird_version", "0.6.2")
         set("jsoup_version", "1.15.3")
         set("bouncy_castle_version", "1.82")
