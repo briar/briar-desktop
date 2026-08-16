01544c6b2790e39c45f22a301da2fda7f329814c
Sebastian Kürten <sebastian@mobanisto.de>
2026-08-16 09:06:09 +0200

    Bump version code to 0.6.6

--- briar-desktop/build.gradle.kts
+++ briar-desktop/build.gradle.kts
@@ -79,7 +79,7 @@ sourceSets {
     }
 }
 
-val versionCode = "0.6.5"
+val versionCode = "0.6.6"
 val buildType = if (project.hasProperty("buildType")) project.properties["buildType"] else "snapshot"
 group = "app.briar.desktop"
 version = "$versionCode-$buildType"
